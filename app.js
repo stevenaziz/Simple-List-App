233751f40ca21e3db50f79faf1cae4675ecba9ec
Steven Aziz <16842077+stevenaziz@users.noreply.github.com>
2022-11-11 15:51:21 -0800

    Implemented server-side post request handling

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ app.get("/api", async (req,res) => {
 })
 
 app.post("/api", async (req,res) => {
-
+  const [data] = fm.ReadData;
+  data[data.length] = JSON.parse(req.body);
+  fm.WriteData(data);
+  res.status(200).send(data);
 })
 
 // page not found route
